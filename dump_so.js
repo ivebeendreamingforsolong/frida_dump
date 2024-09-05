041aff1279cd0bf1c10d6ffe1557c33eaeb76460
maxchou <sirius.chou@gmail.com>
2024-09-05 14:38:46 +0300

    Update dump_so.js
    fix 128M limit

--- dump_so.js
+++ dump_so.js
@@ -9,10 +9,21 @@ rpc.exports = {
             return -1;
         }
         Memory.protect(ptr(libso.base), libso.size, 'rwx');
-        var libso_buffer = ptr(libso.base).readByteArray(libso.size);
-        libso.buffer = libso_buffer;
-        return libso_buffer;
+        var CHUNK_SIZE = 50 * 1024 * 1024; // 50 MiB
+        var numChunks = Math.ceil(libso.size / CHUNK_SIZE); // 计算总块数
+
+        for (var i = 0; i < numChunks; i++) {
+            var offset = i * CHUNK_SIZE;
+            var size = Math.min(CHUNK_SIZE, libso.size - offset); // 确定每次实际传输的大小
+            var chunk = ptr(libso.base).add(offset).readByteArray(size); // 读取块
+
+            // 发送每块数据
+            send({ chunk_index: i, total_chunks: numChunks }, chunk);
+        }
+
+        return { status: "complete", totalSize: libso.size };
     },
+
     allmodule: function() {
         return Process.enumerateModules()
     },
